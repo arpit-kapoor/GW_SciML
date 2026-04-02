ebdaec5e083a89a51a5903631abc299374ddf068
arpit-kapoor <kapoor.arpit97@gmail.com>
2026-04-03 07:50:01 +1100

    Add multiprocess capability to patch creation notebook for efficiency

--- hpc/configs/gino/dynamic_sampling.sh
+++ hpc/configs/gino/dynamic_sampling.sh
@@ -3,7 +3,7 @@ TRAIN_ARGS="
 --base-data-dir ${BASE_DATA_DIR}
 --patch-data-subdir patch_all_ts
 --target-cols mass_concentration head 
---epochs 400
+--epochs 450
 --batch-size 512 
 --input-window-size 5 
 --output-window-size 1 
@@ -12,7 +12,7 @@ TRAIN_ARGS="
 --lr-scheduler-interval 4
 --lr-gamma 0.98
 --grad-clip-norm 1.0 
---save-checkpoint-every 2
+--save-checkpoint-every 10
 --lambda-conc-focus 0.3
 --resolution-ratio 0.60
 --min-resolution-ratio 0.20
